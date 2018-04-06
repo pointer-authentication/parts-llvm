abbc5571038c2cff93ded2212e6cbfa0050bd33e
Hans Liljestrand <liljestrandh@gmail.com>
2018-04-06 15:01:29 +0300

    scripts for build setup

--- pa_cmake_nodebug.sh
+++ pa_cmake_nodebug.sh
@@ -2,7 +2,7 @@
 
 # Install will be done to ${INSTALL_DIR}/llvm${REL_NAME}
 INSTALL_DIR=$HOME/opt/llvm
-REL_NAME=-rel60
+REL_NAME=-rel60_nodebug
 
 # Set the compiler+tools to use
 BUILD_TOOL=Ninja
@@ -13,7 +13,7 @@ CXX_COMPILER=clang++
 # PARALLEL_LINK_JOBS=3
 # PARALLEL_COMPILE_JOBS=3
 
-BUILD_TARGETS="X86"
+BUILD_TARGETS="X86;ARM;AArch64"
 
 declare -r llvm_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 declare -r install_prefix=${INSTALL_DIR}/llvm${REL_NAME}
@@ -22,21 +22,26 @@ declare -r ocaml_install_path=${INSTALL_DIR}/ocaml${REL_NAME}
 cmake -G ${BUILD_TOOL} \
     -DCMAKE_INSTALL_PREFIX=${install_prefix} \
     -DLLVM_OCAML_INSTALL_PATH=${ocaml_install_path} \
-    -DCMAKE_BUILD_TYPE=Relese \
+    -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_C_COMPILER=${C_COMPILER} \
     -DCMAKE_CXX_COMPILER=${CXX_COMPILER} \
     -DLLDB_TEST_C_COMPILER=${C_COMPILER} \
     -DLLDB_TEST_CXX_COMPILER=${CXX_COMPILER} \
-    -DLLVM_TARGETS_TO_BUILD=${BUILD_TARGETS} \
+    -DLLVM_TARGETS_TO_BUILD="${BUILD_TARGETS}" \
     -DLLVM_ENABLE_CXX1Y=On \
     -LLVM_DENABLE_LLD=On \
     -DLLVM_CCACHE_BUILD=On \
     -DLLVM_PARALLEL_LINK_JOBS=${PARALLEL_LINK_JOBS} \
     -DLLVM_PARALLEL_COMPILE_JOBS=${PARALLEL_COMPILE_JOBS} \
     -DLLVM_BUILD_TESTS=Off \
-    -DLLVM_BUILD_EXAMPKLES=Off \
-    -DLLVM_INCLUDE_EXAMPKLES=Off \
+    -DLLVM_BUILD_EXAMPLES=Off \
+    -DLLVM_INCLUDE_EXAMPLES=Off \
     -DLLVM_ENABLE_LTO=On \
     -DLLVM_BUILD_DOCS=Off \
     -DLLVM_ENABLE_DOXYGEN=Off \
+    -DLLVM_ENABLE_RTTI=On \
+    -DLLVM_USE_LINKER=gold \
+    -DLLVM_INSTALL_BINUTILS_SYMLINKS=On \
+    -DLLVM_INSTALL_CCTOOLS_SYMLINKS=On \
+    -DLLVM_BINUTILS_INCDIR=/usr/lib/gcc/x86_64-linux-gnu/6/plugin/include/ \
     ${llvm_root}
