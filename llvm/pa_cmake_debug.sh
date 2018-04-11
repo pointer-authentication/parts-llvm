6424012f7c748346c13ed453ce6508ac4f823329
Hans Liljestrand <liljestrandh@gmail.com>
2018-04-11 11:26:51 +0300

    cmake_script: spit out options (e.g., for CLion)

--- llvm/pa_cmake_debug.sh
+++ llvm/pa_cmake_debug.sh
@@ -8,6 +8,7 @@ REL_NAME=-rel60_debug
 BUILD_TOOL=Ninja
 C_COMPILER=clang
 CXX_COMPILER=clang++
+swig_executable=${HOME}/opt/swig/bin/swig
 
 # Set to limit parallel workloads
 # PARALLEL_LINK_JOBS=3
@@ -19,29 +20,38 @@ declare -r llvm_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 declare -r install_prefix=${INSTALL_DIR}/llvm${REL_NAME}
 declare -r ocaml_install_path=${INSTALL_DIR}/ocaml${REL_NAME}
 
-cmake -G ${BUILD_TOOL} \
-    -DCMAKE_INSTALL_PREFIX=${install_prefix} \
-    -DLLVM_OCAML_INSTALL_PATH=${ocaml_install_path} \
-    -DCMAKE_BUILD_TYPE=Debug \
-    -DCMAKE_C_COMPILER=${C_COMPILER} \
-    -DCMAKE_CXX_COMPILER=${CXX_COMPILER} \
-    -DLLDB_TEST_C_COMPILER=${C_COMPILER} \
-    -DLLDB_TEST_CXX_COMPILER=${CXX_COMPILER} \
-    -DLLVM_TARGETS_TO_BUILD="${BUILD_TARGETS}" \
-    -DLLVM_ENABLE_CXX1Y=On \
-    -LLVM_DENABLE_LLD=On \
-    -DLLVM_CCACHE_BUILD=On \
-    -DLLVM_PARALLEL_LINK_JOBS=${PARALLEL_LINK_JOBS} \
-    -DLLVM_PARALLEL_COMPILE_JOBS=${PARALLEL_COMPILE_JOBS} \
-    -DLLVM_BUILD_TESTS=On \
-    -DLLVM_BUILD_EXAMPLES=Off \
-    -DLLVM_INCLUDE_EXAMPLES=Off \
-    -DLLVM_ENABLE_LTO=Off \
-    -DLLVM_BUILD_DOCS=On \
-    -DLLVM_ENABLE_DOXYGEN=Off \
-    -DLLVM_ENABLE_RTTI=On \
-    -DLLVM_USE_LINKER=gold \
-    -DLLVM_INSTALL_BINUTILS_SYMLINKS=On \
-    -DLLVM_INSTALL_CCTOOLS_SYMLINKS=On \
-    -DLLVM_BINUTILS_INCDIR=/usr/lib/gcc/x86_64-linux-gnu/6/plugin/include/ \
-    ${llvm_root}
+args="
+    -DSWIG_EXECUTABLE=${swig_executable}
+    -DCMAKE_INSTALL_PREFIX=${install_prefix}
+    -DLLVM_OCAML_INSTALL_PATH=${ocaml_install_path}
+    -DCMAKE_BUILD_TYPE=Debug
+    -DCMAKE_C_COMPILER=${C_COMPILER}
+    -DCMAKE_CXX_COMPILER=${CXX_COMPILER}
+    -DLLDB_TEST_C_COMPILER=${C_COMPILER}
+    -DLLDB_TEST_CXX_COMPILER=${CXX_COMPILER}
+    -DLLVM_TARGETS_TO_BUILD=${BUILD_TARGETS}
+    -DLLVM_ENABLE_CXX1Y=On
+    -LLVM_DENABLE_LLD=On
+    -DLLVM_CCACHE_BUILD=On
+    -DLLVM_PARALLEL_LINK_JOBS=${PARALLEL_LINK_JOBS}
+    -DLLVM_PARALLEL_COMPILE_JOBS=${PARALLEL_COMPILE_JOBS}
+    -DLLVM_BUILD_TESTS=On
+    -DLLVM_BUILD_EXAMPLES=Off
+    -DLLVM_INCLUDE_EXAMPLES=Off
+    -DLLVM_ENABLE_LTO=Off
+    -DLLVM_BUILD_DOCS=On
+    -DLLVM_ENABLE_DOXYGEN=Off
+    -DLLVM_ENABLE_RTTI=On
+    -DLLVM_USE_LINKER=gold
+    -DLLVM_INSTALL_BINUTILS_SYMLINKS=On
+    -DLLVM_INSTALL_CCTOOLS_SYMLINKS=On
+    -DLLVM_BINUTILS_INCDIR=/usr/lib/gcc/x86_64-linux-gnu/6/plugin/include/
+    "
+
+if [[ $1 = args ]]; then
+    printf "%s\n" $args
+else
+    echo "nope"
+    set -x
+    cmake -G ${BUILD_TOOL} $args ${llvm_root}
+fi
