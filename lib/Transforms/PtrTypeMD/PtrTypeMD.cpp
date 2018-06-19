e2e29652378da59dea8c4a4e8195024b05b95dc7
Hans Liljestrand <hans.liljestrand@aalto.fi>
2018-06-19 19:20:05 +0300

    wip

--- lib/Transforms/PtrTypeMD/PtrTypeMD.cpp
+++ lib/Transforms/PtrTypeMD/PtrTypeMD.cpp
@@ -38,20 +38,37 @@ namespace {
                     //errs() << "Instruction: ";
                     //I.dump();
                     if (I.getOpcode()==Instruction::Load || I.getOpcode()==Instruction::Store ){
-                        errs() << "\n";
-                        errs() << "Instruction: ";
+                        errs() << "Instruction (operands " << I.getNumOperands() << "): ";
                         I.dump();
-                        errs().write_escaped(I.getOpcodeName(I.getOpcode()));
-                        errs() << "\n";
-                        Type * Ty = I.getOperand(0)->getType();
-                        if(Ty->isPointerTy()){
+
+                        auto opIndex = 0; //(I.getOpcode() == Instruction::Load ? 1 : 0);
+
+                        auto op = I.getOperand(opIndex);
+
+                        if (I.getNumOperands() == 1) {
+                            if (dyn_cast<User>(op)->getNumOperands() == 0) {
+                                errs() << "\t----skipping almost pointer!!!\n";
+                                continue;
+                            }
+
+                            op = dyn_cast<User>(op)->getOperand(0);
+                        }
+
+                        errs() << "\tOperand0: ";
+                        op->dump();
+
+                        Type *Ty = op->getType();
+
+                        if(Ty->isPointerTy()) {
                             if (PointerType * PT = dyn_cast<PointerType>(Ty)) {
                                 Type* ty=PT->getElementType();
                                 //errs()<<PT->getElementType()<<"\n";
                                 bool fty= PT->getElementType()->isFunctionTy();
-                                errs()<<fty<<"\n";
-                                errs()<<"Pointer Type:"<<"\n";
+
+                                /* errs()<<fty<<"\n"; */
+                                errs()<<"\t++++Pointer Type: ";
                                 ty->dump();
+
                                 std::string type_str;
                                 llvm::raw_string_ostream rso(type_str);
                                 ty->print(rso);
@@ -59,11 +76,16 @@ namespace {
                                 Metadata* vals[2]={MDString::get(C, rso.str()), MDString::get(C, std::string (std::to_string(fty)))};
                                 MDNode *N = MDNode::get(C,vals);
                                 I.setMetadata("PAData", N);
-                                errs() << "Metadata:"<<"\n";
-                                errs() << cast<MDString>(I.getMetadata("PAData")->getOperand(0))->getString()<<"\n";
-                                errs() << cast<MDString>(I.getMetadata("PAData")->getOperand(1))->getString()<<"\n";
-                                errs()<<"\n";
+
+                                /* errs() << "Metadata:"<<"\n"; */
+                                /* errs() << cast<MDString>(I.getMetadata("PAData")->getOperand(0))->getString()<<"\n"; */
+                                /* errs() << cast<MDString>(I.getMetadata("PAData")->getOperand(1))->getString()<<"\n"; */
+                                /* errs()<<"\n"; */
+                            } else {
+                                errs() << "\t----dyn_cast fail\n";
                             }
+                        } else {
+                            errs() << "\t----not a pointer ty\n";
                         }
                     }
                 }
