145e7480edd14a7050f7782f4f28da801ad564ca
Hans Liljestrand <hans.liljestrand@aalto.fi>
2018-07-08 19:11:22 +0300

    remove PaSimpleLr pass

--- lib/Target/AArch64/AArch64.h
+++ lib/Target/AArch64/AArch64.h
@@ -54,7 +54,6 @@ InstructionSelector *
 createAArch64InstructionSelector(const AArch64TargetMachine &,
                                  AArch64Subtarget &, AArch64RegisterBankInfo &);
 
-FunctionPass *createAArch64PaSimpleLrPass();
 FunctionPass *createAArch64PaForwardCfiPass();
 
 void initializeAArch64A53Fix835769Pass(PassRegistry&);
