598a89031adcfa23020b8c9adbfc21009035dfb0
Hans Liljestrand <hans.liljestrand@aalto.fi>
2018-08-16 03:27:12 +0300

    use PartsTypeMetadata in opt passes

--- lib/Transforms/PtrTypeMD/PtrTypeMD.cpp
+++ lib/Transforms/PtrTypeMD/PtrTypeMD.cpp
@@ -14,6 +14,7 @@
 //===----------------------------------------------------------------------===//
 
 #include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/PartsTypeMetadata.h>
 #include "llvm/ADT/Statistic.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Instructions.h"
@@ -40,27 +41,15 @@ struct PtrTypeMDPass : public FunctionPass {
 
   bool runOnFunction(Function &F) override;
 
-  //void addToStore(Function &F, Instruction &I);
-  //void addToLoad(Function &F, Instruction &I);
-
-  /** Function to assert the types of instruction operands to be equal */
-  //void assertPtrType(Instruction &I);
-
-  /** Function to set metadata on an instruction */
-  //void md_fn(Function &F, Instruction &I, Type* ptrTy, bool fty);
-
-  //Function to check if the pointer of an instruction operand points to a function instead of data
-  //Type* isFnType(Type* Ty);
-
-  //Function to insert NOP instructions
-  //void insertNops(Function &F, Instruction &I);
+  PartsTypeMetadata_ptr createCallMetadata(Function &F, Instruction &I);
+  PartsTypeMetadata_ptr createLoadMetadata(Function &F, Instruction &I);
+  PartsTypeMetadata_ptr createStoreMetadata(Function &F, Instruction &I);
 };
 
 } // anonyous namespace
 
 char PtrTypeMDPass::ID = 0;
 static RegisterPass<PtrTypeMDPass> X("ptr-type-md-pass", "Pointer Type Metadata Pass");
-
 
 bool PtrTypeMDPass::runOnFunction(Function &F) {
   DEBUG_PA_OPT(&F, do { errs() << TAG << "Function: "; errs().write_escaped(F.getName()) << "\n"; } while(false));
@@ -75,36 +64,32 @@ bool PtrTypeMDPass::runOnFunction(Function &F) {
 
       const auto IOpcode = I.getOpcode();
 
-      MDNode *MD = nullptr;
+      PartsTypeMetadata_ptr MD = nullptr;
 
       switch(IOpcode) {
         case Instruction::Store:
-          assert(isa<StoreInst>(I));
-
-          MD = createPauthMDNode(C, I, I.getOperand(0)->getType());
+          MD = createStoreMetadata(F, I);
           break;
         case Instruction::Load:
-          MD = createPauthMDNode(C, I, I.getType());
+          MD = createLoadMetadata(F, I);
           break;
         case Instruction::Call:
-
-          if (isa<CallInst>(I)) {
-            const CallInst *CI = dyn_cast<CallInst>(&I);
-            if (CI->getCalledFunction() == nullptr) {
-              DEBUG_PA_OPT(&F, errs() << TAG << KGRN << "\t\t\t found indirect call!!!!\n");
-              MD = createPauthMDNode(C, I.getOperand(0)->getType());
-            } else {
-              MD = createPauthMDNode(C, type_id_Ignore);
-            }
-          }
+          MD = createCallMetadata(F, I);
           break;
         default:
           break;
       }
 
       if (MD != nullptr) {
-        I.setMetadata(Pauth_MDKind, MD);
-        DEBUG_PA_OPT(&F, do { errs() << TAG << KGRN << "\t\t\t adding metadata (type_id=" << getPauthTypeId(MD) << ") => "; I.getMetadata(Pauth_MDKind)->dump(); } while(0));
+        MD->attach(C, I);
+
+        DEBUG_PA_OPT(&F, do {
+          if (MD->isIgnored()) {
+            DEBUG_PA_OPT(&F, errs() << TAG << KCYN << "\t\t\t adding metadata to ignore\n");
+          } else {
+            DEBUG_PA_OPT(&F, errs() << TAG << KGRN << "\t\t\t adding metadata (type_id=" << MD->getTypeId() << ")\n");
+          }
+        } while(0));
       } else {
         DEBUG_PA_OPT(&F, errs() << TAG << "\t\t\t skipping\n");
       }
@@ -114,78 +99,74 @@ bool PtrTypeMDPass::runOnFunction(Function &F) {
   return true;
 }
 
-/*
-void PtrTypeMDPass::assertPtrType(Instruction &I)
-{
-  int ops=I.getNumOperands();
-  Type* Ty=I.getOperand(0)->getType();
-  Type* found_ty=isFnType(Ty);
+PartsTypeMetadata_ptr PtrTypeMDPass::createLoadMetadata(Function &F, Instruction &I) {
+  assert(isa<LoadInst>(I));
 
-  for (int i=1; i<ops; i++){
-    Type* next_ty=I.getOperand(i)->getType();
-    Type* next_found_ty=isFnType(next_ty);
-    if (found_ty!=next_found_ty){
-      errs()<<"Exception: Type mismatch between operands\n";
-      break;
-    }
-    else{
-      found_ty=next_found_ty;
+  auto MD = PartsTypeMetadata::get(I.getType());
+
+  if (MD->isCodePointer()) {
+    // Ignore all loaded function-pointers (at least for now)
+    MD->setIgnored(true);
+  }
+
+  return MD;
+}
+
+PartsTypeMetadata_ptr PtrTypeMDPass::createStoreMetadata(Function &F, Instruction &I) {
+  assert(isa<StoreInst>(I));
+
+  auto MD = PartsTypeMetadata::get(I.getOperand(0)->getType());
+
+  if (MD->isCodePointer()) {
+    // We need to ignore code-pointers, unless they are freshly "created" from a function
+    auto *S = dyn_cast<StoreInst>(&I);
+    // The function address is a constant
+    const auto *constant = dyn_cast<Constant>(S->getValueOperand());
+    // Also ignore NULL pointers, they are unusable anyway
+    if (constant == nullptr || constant->isZeroValue()) {
+      MD->setIgnored(true);
     }
   }
-  md.setPtrType(found_ty);
-}
-void PtrTypeMDPass::md_fn(Function &F, Instruction &I, Type* ptrTy, bool fty) {
-  auto &C = F.getContext();
 
-  std::string type_str;
-  llvm::raw_string_ostream rso(type_str);
-  ptrTy->print(rso);
-
-  Metadata* vals[2] = {
-      MDString::get(C, rso.str()),
-      MDString::get(C, std::string(std::to_string(fty)))
-  };
-
-  MDNode *N = MDNode::get(C,vals);
-  I.setMetadata("PAData", N);
-
-  DEBUG_PA_OPT(&F, errs() << TAG << "\t\t\tSetting metadata: ");
-  DEBUG_PA_OPT(&F, errs() << TAG << cast<MDString>(I.getMetadata("PAData")->getOperand(0))->getString() << " ");
-  DEBUG_PA_OPT(&F, errs() << TAG << cast<MDString>(I.getMetadata("PAData")->getOperand(1))->getString() << "\n");
+  return MD;
 }
 
-//Function to check if the pointer of an instruction operand points to a function instead of data
-Type* PtrTypeMDPass::isFnType(Type* Ty){
-  if (PointerType * PT = dyn_cast<PointerType>(Ty)) {
-    Type* ty=PT;
-    //bool fty=false; //is function pointer type or not
-    if (PointerType* pt = dyn_cast<PointerType>(ty)) {
-      int i=0;
-      do {
-        Type* pointedType = pt->getPointerElementType();
-        //errs()<<"Iteration:";
-        //errs()<<i<<"\n";
-        ++i;
-        if (pointedType->isFunctionTy()) {
-          //errs()<<"Found function pointer type"<<"\n";
-          md.setFPtrType(pointedType->isFunctionTy());
-        }
-        // This may be a pointer to a pointer to ...
-        ty = pointedType;
-      } while ((pt = dyn_cast<PointerType>(ty)));
-    }
-    return ty;
+PartsTypeMetadata_ptr PtrTypeMDPass::createCallMetadata(Function &F, Instruction &I) {
+  assert(isa<CallInst>(I));
+
+  PartsTypeMetadata_ptr MD;
+
+  const CallInst *CI = dyn_cast<CallInst>(&I);
+  if (CI->getCalledFunction() == nullptr) {
+    DEBUG_PA_OPT(&F, errs() << TAG << KGRN << "\t\t\t found indirect call!!!!\n");
+    MD = PartsTypeMetadata::get(I.getOperand(0)->getType());
+  } else {
+    MD = PartsTypeMetadata::getIgnored();
   }
-  return Ty;
-}
 
-//Function to insert NOP instructions
-void PtrTypeMDPass::insertNops(Function &F, Instruction &I){
-  auto &C= F.getContext();
-  Value* zero = ConstantInt::get(Type::getInt32Ty(C),0);
-  auto* newInst = BinaryOperator::Create(Instruction::Add, zero, zero, "nop", I.getNextNode());
-  MDNode *N= MDNode::get(C, MDString::get(C, "Nop Instruction"));
-  newInst->setMetadata("PAData", N);
-}
+  errs() << "op num: " << CI->getNumOperands() << "\n";
 
-*/
+  CI->dump();
+
+  // Look at function arguments to see if we need to fix any of them
+  for (auto i = 0U; i < CI->getNumOperands()-1; i++) {
+    auto O =CI->getOperand(i);
+
+    auto V = dyn_cast<Value>(O);
+    V->getType()->dump();
+
+    auto argMD = PartsTypeMetadata::get(V->getType());
+
+    if (!(argMD->isCodePointer() || isa<GlobalValue>(O))) {
+      continue;
+    }
+
+    // lets replace the argument with an PACed pointer here!!!!
+    llvm_unreachable("unimplemented: no support yet for adding PACs at IR level, in this case for function args");
+    // FIXME: need to imlement this with intrinsics that allow us to create a PACed input pointer for the function
+    // Note: in this specific case this is quite acceptable because we are plan to have just a single
+    // "pointer creation" for code pointers.
+  }
+
+  return MD;
+}
