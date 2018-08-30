dece6ba4ff34d0b6adda452faf34c6e4607c825a
Hans Liljestrand <hans.liljestrand@aalto.fi>
2018-08-30 16:30:54 +0300

    move log macros to PartsLog

--- include/llvm/PARTS/PartsLog.h
+++ include/llvm/PARTS/PartsLog.h
@@ -9,6 +9,84 @@
 #include <memory>
 #include "llvm/PARTS/PartsLogStream.h"
 
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+
+
+//#define DISABLE_PA_DEBUG 1
+//#define DEBUG_ONLY_FUNC "check_user"
+//#define DEBUG_ONLY_FUNC "DivideInternalFPF"
+#define DEBUG_PA_OPT(F,x) do { \
+  x;\
+  errs() << KNRM; \
+} while(0);
+#define DEBUG_PA_MIR(F,x) do { \
+  x;\
+  errs() << KNRM; \
+} while(0);
+#define DEBUG_PA_LOW(F,x) do { \
+  x;\
+  errs() << KNRM; \
+} while(0);
+#define DEBUG_PA_FUNC(F, name, x) do { \
+  if (((F)->getName() == (name)) { \
+    errs() << KBLU; \
+    x; \
+    errs() << KNRM; \
+  } \
+} while(0);
+
+
+#ifdef DEBUG_ONLY_FUNC
+#undef DEBUG_PA_OPT
+#undef DEBUG_PA_MIR
+#undef DEBUG_PA_LOW
+#undef DEBUG_PA_FUNC
+#define DEBUG_PA_OPT(F,x) do { \
+  if (F != nullptr && DEBUG_ONLY_FUNC == (F)->getName()) { \
+    x;\
+    errs() << KNRM; \
+  } \
+} while(0);
+#define DEBUG_PA_MIR(F,x) do { \
+  if (F != nullptr && DEBUG_ONLY_FUNC == (F)->getName()) { \
+    x;\
+    errs() << KNRM; \
+  } \
+} while(0);
+#define DEBUG_PA_LOW(F,x) do { \
+  if (F != nullptr && DEBUG_ONLY_FUNC == (F)->getName()) { \
+    x;\
+    errs() << KNRM; \
+  } \
+} while(0);
+#define DEBUG_PA_FUNC(F, name, x) do { \
+  if (DEBUG_ONLY_FUNC == (F)->getName()) { \
+    errs() << KBLU; \
+    x; \
+    errs() << KNRM; \
+  } \
+} while(0);
+#endif
+
+#ifdef DISABLE_PA_DEBUG
+#undef DEBUG_PA_OPT
+#define DEBUG_PA_OPT(F,x)
+#undef DEBUG_PA_MIR
+#define DEBUG_PA_MIR(F,x)
+#undef DEBUG_PA_LOW
+#define DEBUG_PA_LOW(F,x)
+#undef DEBUG_PA_FUNC
+#define DEBUG_PA_FUNC(F,x)
+#endif
+
+
 namespace llvm {
 
 namespace PARTS {
--- lib/Target/AArch64/PointerAuthentication.h
+++ lib/Target/AArch64/PointerAuthentication.h
@@ -8,107 +8,20 @@
 #ifndef POINTERAUTHENTICATION_H
 #define POINTERAUTHENTICATION_H
 
-#define KNRM  "\x1B[0m"
-#define KRED  "\x1B[31m"
-#define KGRN  "\x1B[32m"
-#define KYEL  "\x1B[33m"
-#define KBLU  "\x1B[34m"
-#define KMAG  "\x1B[35m"
-#define KCYN  "\x1B[36m"
-#define KWHT  "\x1B[37m"
-
 #include <string>
 #include "llvm/CodeGen/FastISel.h"
 #include "llvm/CodeGen/MachineBasicBlock.h"
 #include "llvm/CodeGen/MachineInstr.h"
 #include "llvm/CodeGen/MachineInstrBuilder.h"
 #include "llvm/CodeGen/TargetLowering.h"
+#include "llvm/PARTS/PartsLog.h"
 
 #define ENABLE_PAUTH_SLLOW true
-
-//#define DISABLE_PA_DEBUG 1
-//#define DEBUG_ONLY_FUNC "check_user"
-//#define DEBUG_ONLY_FUNC "DivideInternalFPF"
-#define DEBUG_PA_OPT(F,x) do { \
-  x;\
-  errs() << KNRM; \
-} while(0);
-#define DEBUG_PA_MIR(F,x) do { \
-  x;\
-  errs() << KNRM; \
-} while(0);
-#define DEBUG_PA_LOW(F,x) do { \
-  x;\
-  errs() << KNRM; \
-} while(0);
-#define DEBUG_PA_FUNC(F, name, x) do { \
-  if (((F)->getName() == (name)) { \
-    errs() << KBLU; \
-    x; \
-    errs() << KNRM; \
-  } \
-} while(0);
-
-
-#ifdef DEBUG_ONLY_FUNC
-#undef DEBUG_PA_OPT
-#undef DEBUG_PA_MIR
-#undef DEBUG_PA_LOW
-#undef DEBUG_PA_FUNC
-#define DEBUG_PA_OPT(F,x) do { \
-  if (F != nullptr && DEBUG_ONLY_FUNC == (F)->getName()) { \
-    x;\
-    errs() << KNRM; \
-  } \
-} while(0);
-#define DEBUG_PA_MIR(F,x) do { \
-  if (F != nullptr && DEBUG_ONLY_FUNC == (F)->getName()) { \
-    x;\
-    errs() << KNRM; \
-  } \
-} while(0);
-#define DEBUG_PA_LOW(F,x) do { \
-  if (F != nullptr && DEBUG_ONLY_FUNC == (F)->getName()) { \
-    x;\
-    errs() << KNRM; \
-  } \
-} while(0);
-#define DEBUG_PA_FUNC(F, name, x) do { \
-  if (DEBUG_ONLY_FUNC == (F)->getName()) { \
-    errs() << KBLU; \
-    x; \
-    errs() << KNRM; \
-  } \
-} while(0);
-#endif
-
-#ifdef DISABLE_PA_DEBUG
-#undef DEBUG_PA_OPT
-#define DEBUG_PA_OPT(F,x)
-#undef DEBUG_PA_MIR
-#define DEBUG_PA_MIR(F,x)
-#undef DEBUG_PA_LOW
-#define DEBUG_PA_LOW(F,x)
-#undef DEBUG_PA_FUNC
-#define DEBUG_PA_FUNC(F,x)
-#endif
 
 #define Pauth_ModifierReg AArch64::X23
 
 namespace llvm {
 namespace PA {
-
-/*
-class TypeID : public MDNode {
-public:
-  uint64_t
-
-  uint64_t type_id = 0;
-  bool unknown = true;
-  bool pointer = false;
-  bool data = false;
-};
-*/
 
 typedef uint64_t pauth_type_id;
 typedef uint64_t pauth_function_id;
