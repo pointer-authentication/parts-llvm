b0c4f05d426bf653940aa9e855d5746e675ee671
Hans Liljestrand <hans.liljestrand@aalto.fi>
2018-08-30 16:30:54 +0300

    move log macros to PartsLog

--- llvm/lib/PARTS/PartsLog.cpp
+++ llvm/lib/PARTS/PartsLog.cpp
@@ -2,15 +2,6 @@
 #include <unordered_map>
 #include "llvm/PARTS/PartsLog.h"
 
-// #define KNRM  "\x1B[0m"
-// #define KRED  "\x1B[31m"
-// #define KGRN  "\x1B[32m"
-// #define KYEL  "\x1B[33m"
-// #define KBLU  "\x1B[34m"
-// #define KMAG  "\x1B[35m"
-// #define KCYN  "\x1B[36m"
-// #define KWHT  "\x1B[37m"
-
 namespace llvm {
 
 namespace PARTS {
