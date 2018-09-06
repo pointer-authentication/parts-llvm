f02870dc4eb667003c79cd632814439f321e41e4
Hans Liljestrand <hans.liljestrand@aalto.fi>
2018-09-06 14:43:20 +0300

    add logging object for keepin track of stuff

--- llvm/include/llvm/PARTS/PartsLogStream.h
+++ llvm/include/llvm/PARTS/PartsLogStream.h
@@ -13,8 +13,11 @@
 
 #include <string>
 #include <memory>
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/Module.h"
 #include "llvm/PARTS/PartsLogStream.h"
 #include "llvm/IR/Instruction.h"
+#include "llvm/CodeGen/MachineBasicBlock.h"
 
 namespace llvm {
 
@@ -22,20 +25,23 @@ namespace PARTS {
 
 class PartsLogStream {
 private:
-  const std::string m_preamble;
+  llvm::raw_ostream *m_ostream;
 
 public:
-  PartsLogStream() = delete;
+  explicit PartsLogStream(llvm::raw_ostream &ostream);
+  ~PartsLogStream();
 
-  PartsLogStream(const std::string &preamble);
+  PartsLogStream &resetColor();
+  PartsLogStream &changeColor(enum raw_ostream::Colors colors, bool bold,bool bg);
 
-  PartsLogStream &operator<<(std::string &str);
-
+  PartsLogStream &operator<<(const std::string &str);
   PartsLogStream &operator<<(unsigned long &str);
-
+  PartsLogStream &operator<<(const char *str);
   PartsLogStream &operator<<(long &str);
-
+  PartsLogStream &operator<<(unsigned &str);
   PartsLogStream &operator<<(Instruction &I);
+  PartsLogStream &operator<<(Module::global_iterator &GV);
+  PartsLogStream &operator<<(MachineBasicBlock::instr_iterator &MI);
 };
 
 } // namespace PARTS
--- llvm/lib/PARTS/PartsLogStream.cpp
+++ llvm/lib/PARTS/PartsLogStream.cpp
@@ -8,30 +8,72 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include <llvm/PARTS/PartsLogStream.h>
+
 #include "llvm/PARTS/PartsLogStream.h"
 
 namespace llvm {
 
 namespace PARTS {
 
-PartsLogStream::PartsLogStream(const std::string &preamble)
-    : m_preamble(preamble) {}
+PartsLogStream::PartsLogStream(llvm::raw_ostream &ostream)
+    : m_ostream(&ostream) {}
 
-PartsLogStream &PartsLogStream::operator<<(std::string &str) {
+PartsLogStream::~PartsLogStream() {
+  resetColor();
+}
+
+PartsLogStream &PartsLogStream::operator<<(const std::string &str) {
+  *m_ostream << str;
   return *this;
 }
 
 PartsLogStream &PartsLogStream::operator<<(unsigned long &str) {
+  *m_ostream << str;
   return *this;
 }
 
 PartsLogStream &PartsLogStream::operator<<(long &str) {
+  *m_ostream << std::to_string(str);
+  return *this;
+}
+
+PartsLogStream &PartsLogStream::operator<<(unsigned &str) {
+  *m_ostream << std::to_string(str);
   return *this;
 }
 
 PartsLogStream &PartsLogStream::operator<<(Instruction &I) {
+  I.print(*m_ostream, true);
   return *this;
 }
+
+PartsLogStream &PartsLogStream::operator<<(const char *str) {
+  *m_ostream << str;
+  return *this;
+}
+
+PartsLogStream &PartsLogStream::operator<<(Module::global_iterator &GV) {
+  GV->print(*m_ostream, true);
+  return *this;
+}
+
+PartsLogStream &PartsLogStream::operator<<(MachineBasicBlock::instr_iterator &MI) {
+  MI->print(*m_ostream);
+  return *this;
+}
+
+PartsLogStream &PartsLogStream::resetColor() {
+  m_ostream->resetColor();
+  return *this;
+}
+
+PartsLogStream &PartsLogStream::changeColor(enum raw_ostream::Colors color, bool bold, bool bg) {
+  m_ostream->changeColor(color, bold, bg);
+  return *this;
+}
+
+
 
 } // namespace PARTS
 
