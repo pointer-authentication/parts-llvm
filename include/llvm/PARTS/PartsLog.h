9acc06e4244af3c2570cdd1eb0053ae37d1c1833
Hans Liljestrand <hans.liljestrand@aalto.fi>
2018-09-06 14:43:20 +0300

    add logging object for keepin track of stuff

--- include/llvm/PARTS/PartsLog.h
+++ include/llvm/PARTS/PartsLog.h
@@ -13,6 +13,7 @@
 
 #include <string>
 #include <memory>
+#include "llvm/Support/raw_ostream.h"
 #include "llvm/PARTS/PartsLogStream.h"
 
 #define KNRM  "\x1B[0m"
@@ -101,23 +102,86 @@ class PartsLog;
 typedef std::shared_ptr<PartsLog> PartsLog_ptr;
 
 class PartsLog {
-public:
-
 private:
+  class Stats {
+  private:
+    std::map<std::string, int> m_stats;
+
+  public:
+    Stats();
+    Stats(const Stats &) = delete;
+    ~Stats();
+
+    void dump() const;
+    void inc(const std::string var);
+    void dec(const std::string var);
+  };
+
   const std::string m_name;
+  bool m_enabled = true;
+  bool m_onlyFunc = false;
+  std::string m_onlyFuncName = "";
+
+  static Stats &getStats();
+  inline raw_ostream &get_ostream(const raw_ostream::Colors c) const;
+  inline raw_ostream &get_ostream(const std::string &F, const raw_ostream::Colors c) const;
 
 protected:
 public:
-  PartsLog(const std::string &name);
+  explicit PartsLog(const std::string &name);
+  ~PartsLog();
 
-  PartsLogStream &warn();
-  PartsLogStream &info();
-  PartsLogStream &error();
-  PartsLogStream &red();
-  PartsLogStream &green();
+  inline PartsLog &enable();
+  inline PartsLog &disable();
+
+  void restrictToFunc(const std::string func);
+
+  PARTS::PartsLogStream inc(const std::string &var, bool b, const std::string &F = "");
+  PARTS::PartsLogStream inc(const std::string &var, const raw_ostream::Colors c = raw_ostream::BLUE, const std::string &F = "");
+  PARTS::PartsLogStream dec(const std::string &var, bool b, const std::string &F = "");
+  PARTS::PartsLogStream dec(const std::string &var, const raw_ostream::Colors c = raw_ostream::BLUE, const std::string &F = "");
+
+  PARTS::PartsLogStream debug();
+  PARTS::PartsLogStream info();
+  PARTS::PartsLogStream warn();
+  PARTS::PartsLogStream error();
+  PARTS::PartsLogStream red();
+  PARTS::PartsLogStream green();
+
+  PARTS::PartsLogStream debug(const std::string &F);
+  PARTS::PartsLogStream info(const std::string &F);
+  PARTS::PartsLogStream warn(const std::string &F);
+  PARTS::PartsLogStream error(const std::string &F);
+  PARTS::PartsLogStream red(const std::string &F);
+  PARTS::PartsLogStream green(const std::string &F);
 
   static PartsLog_ptr getLogger(const std::string &name);
 };
+
+inline raw_ostream &PartsLog::get_ostream(const raw_ostream::Colors c) const {
+  if (m_enabled)
+    return (errs().changeColor(c, false, false) << m_name << ": ");
+
+  return nulls();
+}
+
+inline raw_ostream &PartsLog::get_ostream(const std::string &F, const raw_ostream::Colors c) const {
+  if (m_enabled && (!m_onlyFunc || F.compare(m_onlyFuncName) == 0)) {
+    return (errs().changeColor(c, false, false) << m_name << ": ");
+  }
+
+  return nulls();
+}
+
+inline PartsLog &PartsLog::enable() {
+  m_enabled = true;
+  return *this;
+}
+
+inline PartsLog &PartsLog::disable() {
+  m_enabled = false;
+  return *this;
+}
 
 } // PARTS
 
--- lib/PARTS/PartsLog.cpp
+++ lib/PARTS/PartsLog.cpp
@@ -8,8 +8,15 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include <unordered_map>
 #include "llvm/PARTS/PartsLog.h"
+
+#include <mutex>
+#include <unordered_map>
+#include <unistd.h>
+#include <llvm/PARTS/PartsLog.h>
+
+#include "llvm/Support/raw_ostream.h"
+
 
 namespace llvm {
 
@@ -18,34 +25,57 @@ namespace PARTS {
 PartsLog::PartsLog(const std::string &name)
     : m_name(name) {}
 
-PartsLogStream &PartsLog::warn() {
-  static PartsLogStream log = PartsLogStream("warning: ");
-  return log;
+PartsLog::~PartsLog() {}
+
+void PartsLog::restrictToFunc(const std::string func) {
+  m_onlyFunc = true;
+  m_onlyFuncName = func;
 }
 
-PartsLogStream &PartsLog::info() {
-  static PartsLogStream log = PartsLogStream("info: ");
-  return log;
+
+PARTS::PartsLogStream PartsLog::debug() {
+  return PartsLogStream(get_ostream(raw_ostream::CYAN));
 }
 
-PartsLogStream &PartsLog::error() {
-  static PartsLogStream log = PartsLogStream("error: ");
-  return log;
+PARTS::PartsLogStream PartsLog::debug(const std::string &F) {
+  return PartsLogStream(get_ostream(F, raw_ostream::CYAN));
 }
 
-PartsLogStream &PartsLog::red() {
-  static PartsLogStream log = PartsLogStream("red: ");
-  return log;
+PARTS::PartsLogStream PartsLog::info() {
+  return PartsLogStream(get_ostream(raw_ostream::WHITE));
 }
 
-PartsLogStream &PartsLog::green() {
-  static PartsLogStream log = PartsLogStream("green: ");
-  return log;
+PARTS::PartsLogStream PartsLog::info(const std::string &F) {
+  return PartsLogStream(get_ostream(F, raw_ostream::WHITE));
 }
+
+PARTS::PartsLogStream PartsLog::warn() {
+  return PartsLogStream(get_ostream(raw_ostream::YELLOW));
+}
+
+PARTS::PartsLogStream PartsLog::error() {
+  return PartsLogStream(get_ostream(raw_ostream::MAGENTA));
+}
+
+PARTS::PartsLogStream PartsLog::error(const std::string &F) {
+  return PartsLogStream(get_ostream(F, raw_ostream::MAGENTA));
+}
+
+PARTS::PartsLogStream PartsLog::red() {
+  return PartsLogStream(get_ostream(raw_ostream::RED));
+}
+
+PARTS::PartsLogStream PartsLog::green() {
+  return PartsLogStream(get_ostream(raw_ostream::GREEN));
+}
+
+std::mutex _mutex;
 
 PartsLog_ptr PartsLog::getLogger(const std::string &name)
 {
   static auto loggers = std::unordered_map<std::string, std::shared_ptr<PartsLog>>();
+
+  std::unique_lock<std::mutex> lock(_mutex);
 
   auto found = loggers.find(name);
   if (found != loggers.end()) {
@@ -56,6 +86,72 @@ PartsLog_ptr PartsLog::getLogger(const std::string &name)
     return newLogger;
   }
 }
+
+PartsLogStream PartsLog::inc(const std::string &var, bool b, const std::string &F) {
+  return b ? inc(var, raw_ostream::GREEN, F) : inc(var, raw_ostream::RED, F);
+}
+
+PartsLogStream PartsLog::dec(const std::string &var, bool b, const std::string &F) {
+  return b ? dec(var, raw_ostream::GREEN, F) : dec(var, raw_ostream::RED, F);
+}
+
+PartsLogStream PartsLog::inc(const std::string &var, const raw_ostream::Colors c, const std::string &F) {
+  auto &s = getStats();
+  s.inc(var);
+  return PartsLogStream(get_ostream(F, c));
+}
+
+PartsLogStream PartsLog::dec(const std::string &var, const raw_ostream::Colors c, const std::string &F) {
+  auto &s = getStats();
+  s.inc(var);
+  return PartsLogStream(get_ostream(F, c));
+}
+
+
+PartsLog::Stats &PartsLog::getStats() {
+  static Stats stats;
+  return stats;
+}
+
+PartsLog::Stats::Stats() {
+  m_stats = std::map<std::string,int>();
+}
+
+PartsLog::Stats::~Stats() {
+  dump();
+}
+
+void PartsLog::Stats::dump() const {
+  errs() << "\n//---------------- Dumping PartsLog::Stats --------------------//\n";
+
+  if (m_stats.empty())
+    errs() << "EMPTY!!!\n";
+
+  for (auto p : m_stats)
+    errs() << p.first << ": " << p.second << "\n";
+
+  errs() << "//---------------- done ---------------------------------------//\n\n";
+}
+
+void PartsLog::Stats::inc(const std::string var) {
+  auto found = m_stats.find(var);
+  if (found == m_stats.end()) {
+    m_stats.insert({var, 1});
+  } else {
+    found->second++;
+  }
+}
+
+void PartsLog::Stats::dec(const std::string var) {
+  auto found = m_stats.find(var);
+  if (found == m_stats.end()) {
+    m_stats.insert({var, -1});
+  } else {
+    found->second--;
+  }
+}
+
+
 
 } // PARTS
 
