e71e182527a07fce0252fb858ac37acd535d8d31
Carlos Chinea <carlos.chinea.perez@huawei.com>
2019-02-13 16:45:45 +0200

    PartOptCpiPass: Extract a function to handle call
    Extract a function to handle call instructions
    
    Signed-off-by: Carlos Chinea <carlos.chinea.perez@huawei.com>

--- llvm/lib/Transforms/PartsOpt/PartsOptCpiPass.cpp
+++ llvm/lib/Transforms/PartsOpt/PartsOptCpiPass.cpp
@@ -53,6 +53,7 @@ private:
    */
   Value *generatePACedValue(Module *M, Instruction &I, Value *V);
   bool handleInstruction(Function &F, Instruction &I);
+  bool handleCallInstruction(Function &F, Instruction &I);
 };
 
 } // anonymous namespace
@@ -100,85 +101,91 @@ bool PartsOptCpiPass::handleInstruction(Function &F, Instruction &I)
       }
       break;
     }
-    case Instruction::Call: {
-      /*
-       * For functions we need to do two things:
-       * 1: Either sign or unsign pointer args based on function linkage
-       *        e.g., ~ func(printf)        -> func(pacia(printf)))
-       *              ~ qsort(..., compare) -> qsort(..., compare)
-       *              ~ qsort(..., ptr)     -> qsort(..., autia(compare))
-       * 2: Make sure indirect function calls are authenticated.
-       * FIXME: handle pointers InlineAsm
-       * FIXME: handle indirect calls using BitCastOperator (could perhaps be casting function pointer?)
-       */
-      auto CI = dyn_cast<CallInst>(&I);
-      auto CalledFunction = CI->getCalledFunction();
-
-      if (CalledFunction != nullptr && CalledFunction->isDeclaration() &&
-          !CalledFunction->hasFnAttribute("parts-cpi")) {
-        // We assume externally linked function do not have Parts instrumentation, and must, not only
-        // omit PACing of args, but also authenticate and unPAC ingoing signed pointers.
-        //
-        // NOTE: This cannot detect indirect calls to externally linked functions!
-        for (auto i = 0U, end = CI->getNumArgOperands(); i < end; ++i) {
-          auto arg = CI->getArgOperand(i);
-          const auto argType = arg->getType();
-          if (argType->isPointerTy() &&
-              argType->getPointerElementType()->isFunctionTy() &&
-              !isa<Constant>(arg)) {
-
-            IRBuilder<> Builder(&I);
-            auto authenticatedArg =  Builder.CreateCall(
-                Intrinsic::getDeclaration(F.getParent(), Intrinsic::pa_autia, { argType }),
-                { arg, PartsTypeMetadata::idConstantFromType(F.getContext(), argType) }
-            );
-            CI->setArgOperand(i, authenticatedArg);
-          }
-        }
-      } else {
-        // Make sure args are signed for non-externally linked functions
-        for (auto i = 0U, end = CI->getNumArgOperands(); i < end; ++i) {
-          auto paced = generatePACedValue(F.getParent(), I, CI->getArgOperand(i));
-          if (paced != nullptr) {
-            ++StatSignFunctionArg;
-            CI->setArgOperand(i, paced);
-          }
-        }
-      }
-
-      /*
-      const Value *V = getCalledValue();
-      if (!V)
-        return false;
-      if (isa<FunTy>(V) || isa<Constant>(V))
-        return false;
-      if (const CallInst *CI = dyn_cast<CallInst>(getInstruction())) {
-        if (CI->isInlineAsm())
-          return false;
-      }
-      return true;
-       */
-
-      // 2: Handle indirect function calls
-      if (CallSite(CI).isIndirectCall()) {
-        auto calledValue = CI->getCalledValue();
-        const auto calledValueType = calledValue->getType();
-
-        ++StatAuthenticateIndirectCall;
-        // Generate Builder for inserting pa_autcall
-        IRBuilder<> Builder(&I);
-        // Get pa_autia declaration for correct input type
-        auto autcall = Intrinsic::getDeclaration(F.getParent(), Intrinsic::pa_autcall, { calledValueType });
-        // Get type_id as Constant
-        auto typeIdConstant = PartsTypeMetadata::idConstantFromType(F.getContext(), calledValueType);
-        // Insert intrinsics to authenticated the signed function pointer
-        auto paced = Builder.CreateCall(autcall, { calledValue, typeIdConstant }, "");
-
-        // Replace signed pointer with the authenticated one
-        CI->setCalledFunction(paced);
-      }
+    case Instruction::Call:
+      return handleCallInstruction(F, I);
       break;
+  }
+
+  return true;
+}
+
+bool PartsOptCpiPass::handleCallInstruction(Function &F, Instruction &I)
+{
+  /*
+   * For functions we need to do two things:
+   * 1: Either sign or unsign pointer args based on function linkage
+   *        e.g., ~ func(printf)        -> func(pacia(printf)))
+   *              ~ qsort(..., compare) -> qsort(..., compare)
+   *              ~ qsort(..., ptr)     -> qsort(..., autia(compare))
+   * 2: Make sure indirect function calls are authenticated.
+   * FIXME: handle pointers InlineAsm
+   * FIXME: handle indirect calls using BitCastOperator (could perhaps be casting function pointer?)
+   */
+  auto CI = dyn_cast<CallInst>(&I);
+  auto CalledFunction = CI->getCalledFunction();
+
+  if (CalledFunction != nullptr && CalledFunction->isDeclaration() &&
+      !CalledFunction->hasFnAttribute("parts-cpi")) {
+    // We assume externally linked function do not have Parts instrumentation, and must, not only
+    // omit PACing of args, but also authenticate and unPAC ingoing signed pointers.
+    //
+    // NOTE: This cannot detect indirect calls to externally linked functions!
+    for (auto i = 0U, end = CI->getNumArgOperands(); i < end; ++i) {
+      auto arg = CI->getArgOperand(i);
+      const auto argType = arg->getType();
+      if (argType->isPointerTy() &&
+          argType->getPointerElementType()->isFunctionTy() &&
+          !isa<Constant>(arg)) {
+
+        IRBuilder<> Builder(&I);
+        auto authenticatedArg =  Builder.CreateCall(
+            Intrinsic::getDeclaration(F.getParent(), Intrinsic::pa_autia, { argType }),
+            { arg, PartsTypeMetadata::idConstantFromType(F.getContext(), argType) }
+        );
+        CI->setArgOperand(i, authenticatedArg);
+      }
     }
+  } else {
+    // Make sure args are signed for non-externally linked functions
+    for (auto i = 0U, end = CI->getNumArgOperands(); i < end; ++i) {
+      auto paced = generatePACedValue(F.getParent(), I, CI->getArgOperand(i));
+      if (paced != nullptr) {
+        ++StatSignFunctionArg;
+        CI->setArgOperand(i, paced);
+      }
+    }
+  }
+
+  /*
+  const Value *V = getCalledValue();
+  if (!V)
+    return false;
+  if (isa<FunTy>(V) || isa<Constant>(V))
+    return false;
+  if (const CallInst *CI = dyn_cast<CallInst>(getInstruction())) {
+    if (CI->isInlineAsm())
+      return false;
+  }
+  return true;
+   */
+
+  // 2: Handle indirect function calls
+  if (CallSite(CI).isIndirectCall()) {
+    auto calledValue = CI->getCalledValue();
+    const auto calledValueType = calledValue->getType();
+
+    ++StatAuthenticateIndirectCall;
+    // Generate Builder for inserting pa_autcall
+    IRBuilder<> Builder(&I);
+    // Get pa_autia declaration for correct input type
+    auto autcall = Intrinsic::getDeclaration(F.getParent(), Intrinsic::pa_autcall, { calledValueType });
+    // Get type_id as Constant
+    auto typeIdConstant = PartsTypeMetadata::idConstantFromType(F.getContext(), calledValueType);
+    // Insert intrinsics to authenticated the signed function pointer
+    auto paced = Builder.CreateCall(autcall, { calledValue, typeIdConstant }, "");
+
+    // Replace signed pointer with the authenticated one
+    CI->setCalledFunction(paced);
   }
 
   return true;
