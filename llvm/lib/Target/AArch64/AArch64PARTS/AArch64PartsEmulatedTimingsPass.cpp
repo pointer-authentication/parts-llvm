d6b005fddd03dec8b2711df92d77c1daf360cd38
Carlos Chinea <carlos.chinea.perez@huawei.com>
2019-02-27 12:06:12 +0200

    PartEmulatedTimings: Emulate BLRAA and BRAA
    Support timing emulations for BLRAA and BRAA
    
    Signed-off-by: Carlos Chinea <carlos.chinea.perez@huawei.com>

--- llvm/lib/Target/AArch64/AArch64PARTS/AArch64PartsEmulatedTimingsPass.cpp
+++ llvm/lib/Target/AArch64/AArch64PARTS/AArch64PartsEmulatedTimingsPass.cpp
@@ -25,12 +25,10 @@
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/raw_ostream.h"
-#include "llvm/CodeGen/RegisterScavenging.h"
 // PARTS includes
 #include "llvm/PARTS/PartsTypeMetadata.h"
 #include "llvm/PARTS/PartsEventCount.h"
 #include "llvm/PARTS/Parts.h"
-#include "PartsUtils.h"
 
 #define DEBUG_TYPE "AArch64PartsEmulatedTimingPass"
 
@@ -60,10 +58,11 @@ namespace {
   inline bool handleInstruction(MachineFunction &MF, MachineBasicBlock &MBB, MachineBasicBlock::instr_iterator &MIi);
   inline void doMachineFunctionInit(MachineFunction &MF);
   inline bool isPAInstruction(unsigned Opcode);
-//   void replaceBranchByAuthenticatedBranch(MachineBasicBlock &MBB, MachineInstr *MI_indcall, unsigned dst, unsigned mod) override;
   inline void addNops(MachineBasicBlock &MBB, MachineInstr *MI, unsigned ptrReg, unsigned modReg, const DebugLoc &DL);
   inline void replacePACIA(MachineFunction &MF, MachineBasicBlock &MBB, MachineInstr &MI);
   inline void replaceAUTIA(MachineFunction &MF, MachineBasicBlock &MBB, MachineInstr &MI);
+  inline void replaceBranchAuth(MachineFunction &MF, MachineBasicBlock &MBB, MachineInstr &MI);
+  inline const MCInstrDesc &getIndirectCallMachineInstruction(MachineInstr &MI);
   void replacePAInstructionCommon(MachineFunction &MF, MachineBasicBlock &MBB, MachineInstr &MI);
  };
 
@@ -121,11 +120,10 @@ inline bool AArch64PartsEmulatedTimingPass::handleInstruction(MachineFunction &M
     case AArch64::AUTIA:
       replaceAUTIA(MF, MBB, MI);
       break;
-#if 0
-    case AArch64::PARTS_AUTCALL:
-      replaceAUTCALL(MF, MBB, MI);
+    case AArch64::BLRAA:
+    case AArch64::BRAA:
+      replaceBranchAuth(MF, MBB, MI);
       break;
-#endif
   }
 
   MI.removeFromParent(); // Remove pac instruction !
@@ -137,12 +135,10 @@ inline bool AArch64PartsEmulatedTimingPass::isPAInstruction(unsigned Opcode) {
   switch (Opcode) {
     case AArch64::PACIA:
     case AArch64::AUTIA:
-#if 0
-    case AArch64::PARTS_AUTCALL:
-#endif
+    case AArch64::BLRAA:
+    case AArch64::BRAA:
       return true;
   }
-
   return false;
 }
 
@@ -153,74 +149,20 @@ inline void AArch64PartsEmulatedTimingPass::replacePACIA(MachineFunction &MF,
   ++StatPacia;
 }
 
-#if 0
-
-void AArch64PartsEmulatedTimingPass::replaceAUTCALL(MachineFunction &MF,
-                                                   MachineBasicBlock &MBB,
-                                                   MachineInstr &MI) {
-
-  MachineInstr *MI_indcall = findIndirectCallMachineInstr(MI.getNextNode());
-  if (MI_indcall == nullptr)
-    triggerCompilationErrorOrphanAUTCALL(MBB);
-
-  const unsigned mod_orig = MI.getOperand(2).getReg();
-  const unsigned dst = MI.getOperand(0).getReg();
-  const unsigned mod = getFreeRegister(MBB, MI_indcall->getPrevNode(), MI);
-
-  insertMovInstr(MBB, &MI, mod, mod_orig);
-
-  replaceBranchByAuthenticatedBranch(MBB, MI_indcall, dst, mod);
-
-  ++StatAuthBranch;
-}
-
-inline void AArch64PartsEmulatedTimingPass::triggerCompilationErrorOrphanAUTCALL(MachineBasicBlock &MBB) {
-  DEBUG(MBB.dump());
-  llvm_unreachable("failed to find BLR for AUTCALL");
-}
-
-void AArch64PartsEmulatedTimingPass::replaceBranchByAuthenticatedBranch(MachineBasicBlock &MBB,
-                                                                    MachineInstr *MI_indcall,
-                                                                    unsigned dst,
-                                                                    unsigned mod) {
-  auto &MCI = getIndirectCallMachineInstruction(MI_indcall);
-  insertPACInstr(MBB, MI_indcall, dst, mod, MCI);
-  MI_indcall->removeFromParent(); // Remove the replaced BR instruction
-}
-
-inline unsigned AArch64PartsEmulatedTimingPass::getFreeRegister(MachineBasicBlock &MBB,
-                                                     MachineInstr *MI_from,
-                                                     MachineInstr &MI_to) {
-  RegScavenger RS;
-  auto &RC = AArch64::GPR64commonRegClass;
-
-  RS.enterBasicBlockEnd(MBB);
-  RS.backward(MachineBasicBlock::iterator(MI_from));
-  const unsigned reg = RS.scavengeRegisterBackwards(RC, MachineBasicBlock::iterator(MI_to), false, 0);
-  RS.setRegUsed(reg); // Tell the Register Scavenger that the register is alive. Needed !?
-
-  return reg;
-}
-
-inline const MCInstrDesc &AArch64PartsEmulatedTimingPass::getIndirectCallMachineInstruction(MachineInstr *MI_indcall) {
-
-  if (isNormalIndirectCall(MI_indcall))
-   return TII->get(AArch64::BLRAA);
-
-  // This is a tail call return, and we need to use BRAA
-  // (tail-call: ~optimizatoin where a tail-cal is converted to a direct call so that
-  // the tail-called function can return immediately to the current callee, without
-  // going through the currently active function.)
-
- return TII->get(AArch64::BRAA);
-}
-#endif
-
 inline void AArch64PartsEmulatedTimingPass::replaceAUTIA(MachineFunction &MF,
                                                  MachineBasicBlock &MBB,
                                                  MachineInstr &MI) {
   replacePAInstructionCommon(MF, MBB, MI);
   ++StatAutia;
+}
+
+void AArch64PartsEmulatedTimingPass::replaceBranchAuth(MachineFunction &MF,
+                                                   MachineBasicBlock &MBB,
+                                                   MachineInstr &MI) {
+  replacePAInstructionCommon(MF, MBB, MI);
+  auto &MCI = getIndirectCallMachineInstruction(MI);
+  BuildMI(MBB, MI, MI.getDebugLoc(), MCI).add(MI.getOperand(0));
+  ++StatAuthBranch;
 }
 
 void AArch64PartsEmulatedTimingPass::replacePAInstructionCommon(MachineFunction &MF,
@@ -232,17 +174,6 @@ void AArch64PartsEmulatedTimingPass::replacePAInstructionCommon(MachineFunction 
   addNops(MBB, &MI, dst.getReg(), mod.getReg(), MI.getDebugLoc());
 }
 
-#if 0
-void AArch64PartsEmulatedTimingPass::replaceBranchByAuthenticatedBranch(MachineBasicBlock &MBB,
-                                                                                MachineInstr *MI_indcall,
-                                                                                unsigned dst,
-                                                                                unsigned mod) {
- // FIXME: This might break if the pointer is reused elsewhere!!!
- addNops(MBB, MI_indcall, dst, mod, MI_indcall->getDebugLoc());
-}
-
-#endif
-
 inline void AArch64PartsEmulatedTimingPass::addNops(MachineBasicBlock &MBB, MachineInstr *MI, unsigned ptrReg, unsigned modReg, const DebugLoc &DL) {
   BuildMI(MBB, MI, DL, TII->get(AArch64::EORXri), ptrReg).addReg(ptrReg).addImm(17);
   BuildMI(MBB, MI, DL, TII->get(AArch64::EORXri), ptrReg).addReg(ptrReg).addImm(37);
@@ -250,34 +181,10 @@ inline void AArch64PartsEmulatedTimingPass::addNops(MachineBasicBlock &MBB, Mach
   BuildMI(MBB, MI, DL, TII->get(AArch64::EORXrs), ptrReg).addReg(ptrReg).addReg(modReg).addImm(0);
 }
 
-#if 0
-inline MachineInstr *AArch64PartsEmulatedTimingPass::findIndirectCallMachineInstr(MachineInstr *MI) {
-  while (MI != nullptr && !isIndirectCall(*MI))
-    MI = MI->getNextNode();
+inline const MCInstrDesc &AArch64PartsEmulatedTimingPass::getIndirectCallMachineInstruction(MachineInstr &MI) {
 
-  return MI;
-}
+  if (MI.getOpcode() == AArch64::BLRAA)
+      return TII->get(AArch64::BLR);
 
-inline bool AArch64PartsEmulatedTimingPass::isIndirectCall(const MachineInstr &MI) const {
-  switch (MI.getOpcode()) {
-    case AArch64::BLR:        // Normal indirect call
-    case AArch64::TCRETURNri: // Indirect tail call
-      return true;
-  }
-  return false;
+ return TII->get(AArch64::BR);
 }
-
-inline void AArch64PartsEmulatedTimingPass::insertMovInstr(MachineBasicBlock &MBB,
-                                                MachineInstr *MI,
-                                                unsigned dstReg,
-                                                unsigned srcReg) {
-  auto MOVMI = BuildMI(MBB, MI, MI->getDebugLoc(), TII->get(AArch64::ORRXrs), dstReg);
-  MOVMI.addUse(AArch64::XZR);
-  MOVMI.addUse(srcReg);
-  MOVMI.addImm(0);
-}
-
-inline bool AArch64PartsEmulatedTimingPass::isNormalIndirectCall(const MachineInstr *MI) const {
-  return MI->getOpcode() == AArch64::BLR;
-}
-#endif
