537c303d17594daf8d0423a95a9106a0fef6ccba
Carlos Chinea <carlos.chinea.perez@huawei.com>
2019-07-19 12:33:27 +0300

    LLVM: Only build AArch64
    Do not build x86 along AArch64. We only do development
    for AArch64.
    
    Signed-off-by: Carlos Chinea <carlos.chinea.perez@huawei.com>

--- llvm/setup_build.sh
+++ llvm/setup_build.sh
@@ -121,7 +121,7 @@ use_linker=${llvm_dir}/bin/ld.lld
 build_type="Debug"
 
 # Modify this to change target platforms
-BUILD_TARGETS="AArch64;X86"
+BUILD_TARGETS="AArch64"
 
 # Using shared libs should speed up intermediate development.
 build_shared_libs=On # (default Off)
