2af68b14bc4300007c9c13022e340c34a6febc80
Carlos Chinea <carlos.chinea.perez@huawei.com>
2019-07-26 16:14:43 +0300

    clang:PARTS:Builtins: Test bitcasting and constant
    Add tests for handling bitcasting and constant value.
    
    Signed-off-by: Carlos Chinea <carlos.chinea.perez@huawei.com>

--- clang/test/Sema/builtins-aarch64-parts.c
+++ clang/test/Sema/builtins-aarch64-parts.c
@@ -1,12 +1,23 @@
 // RUN: %clang_cc1 -triple aarch64-none-linux-gnu -fsyntax-only -verify %s
 #include <stdint.h>
 
+struct foo;
+
 uint64_t get_pa_modifier(uint64_t *value) {
   uint64_t res;
+
   res = __builtin_arm_parts_modifier(value);
   res |= __builtin_arm_parts_modifier(); // expected-error {{too few arguments to function call}}
   res |= __builtin_arm_parts_modifier(value, value); // expected-error {{too many arguments to function call}}
   res |= __builtin_arm_parts_modifier(*value); // expected-error {{address argument to builtin must be a pointer}}
   res |= __builtin_arm_parts_modifier(&value);
+  res |= __builtin_arm_parts_modifier((const uint64_t *)value);
+  res |= __builtin_arm_parts_modifier((const volatile uint64_t *)value);
+  res |= __builtin_arm_parts_modifier((volatile uint64_t *)value);
+  res |= __builtin_arm_parts_modifier((char *)value);
+  res |= __builtin_arm_parts_modifier((struct foo *)value);
+  res |= __builtin_arm_parts_modifier((unsigned long)value); // expected-error {{address argument to builtin must be a pointer}}
+  res |= __builtin_arm_parts_modifier(42); // expected-error {{address argument to builtin must be a pointer}}
+
   return res;
 }
