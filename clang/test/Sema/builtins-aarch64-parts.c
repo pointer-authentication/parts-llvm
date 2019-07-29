f709713c79f3a01727ee92cf1acaf9d29d70bc3d
Carlos Chinea <carlos.chinea.perez@huawei.com>
2019-07-29 14:59:08 +0300

    clang:PARTS:builtins:tests: Test function pointers
    Test that our intrinsic is capable of handling function
    pointer arguments.
    
    Signed-off-by: Carlos Chinea <carlos.chinea.perez@huawei.com>

--- clang/test/Sema/builtins-aarch64-parts.c
+++ clang/test/Sema/builtins-aarch64-parts.c
@@ -2,6 +2,9 @@
 #include <stdint.h>
 
 struct foo;
+void funcbar(void);
+
+void (*foobar)(void);
 
 uint64_t get_pa_modifier(uint64_t *value) {
   uint64_t res;
@@ -18,6 +21,8 @@ uint64_t get_pa_modifier(uint64_t *value) {
   res |= __builtin_arm_parts_modifier((struct foo *)value);
   res |= __builtin_arm_parts_modifier((unsigned long)value); // expected-error {{address argument to builtin must be a pointer}}
   res |= __builtin_arm_parts_modifier(42); // expected-error {{address argument to builtin must be a pointer}}
+  res |= __builtin_arm_parts_modifier(funcbar);
+  res |= __builtin_arm_parts_modifier(foobar);
 
   return res;
 }
