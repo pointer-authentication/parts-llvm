797702ef4caba687ce8b7e9c44f55f4640636eda
Carlos Chinea <carlos.chinea.perez@huawei.com>
2019-07-30 12:59:46 +0300

    clang:PARTS: Check IR generation different types
    Add extra test cases to test LLVM IR generation for
    different C types.
    
    Signed-off-by: Carlos Chinea <carlos.chinea.perez@huawei.com>

--- clang/test/CodeGen/builtins-arm64-parts.c
+++ clang/test/CodeGen/builtins-arm64-parts.c
@@ -6,3 +6,23 @@ uint64_t get_pa_modifier(uint64_t *value)
   return __builtin_arm_parts_modifier(value);
 // CHECK: call i64 @llvm.pa.modifier.p0i64(i64* %value)
 }
+
+uint64_t get_pa_modifier8(char *value)
+{
+  return __builtin_arm_parts_modifier(value);
+// CHECK: call i64 @llvm.pa.modifier.p0i8(i8* %value)
+}
+
+struct foo;
+
+uint64_t get_pa_modifier_struct(struct foo *value)
+{
+  return __builtin_arm_parts_modifier(value);
+// CHECK: call i64 @llvm.pa.modifier.p0s_struct.foos(%struct.foo* %value)
+}
+
+uint64_t get_pa_modifier_func(void (*value)(void))
+{
+  return __builtin_arm_parts_modifier(value);
+// CHECK: call i64 @llvm.pa.modifier.p0f_isVoidf(void ()* %value)
+}
